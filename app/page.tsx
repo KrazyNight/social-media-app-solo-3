742aeeee75670db5b7c6dae247714d2e274a88fa
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-10-03 14:44:21 -0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <Sidebar />
+    {/* <PostFeed /> 
+    <Widgets /> */}
+    
+    </>
+  );
+}
