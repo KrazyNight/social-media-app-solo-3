f224599c43058d340f32918c8fdc8dad4e72bb22
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-10-03 15:46:58 -0700

    sidebar part 1

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <Sidebar />
-    {/* <PostFeed /> 
-    <Widgets /> */}
+    <div className="text-[#0f1419] min-h-screen
+    border-2 border-black
+    max-w-[1400px] mx-auto
+    ">
+
+      <Sidebar />
+      {/* <PostFeed /> 
+      <Widgets /> */}
+    </div>
     
     </>
   );
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
 
-export default function Sidebar() {
-  return (
-    <>
-      sidebar
-    </>
-  )
-}
+// //step 2
+// import React from 'react'
+// // import {} from "@heroicons/react/24/outline"
+
+// export default function Sidebar() {
+//   return (
+//     <>
+//         <nav>
+//             <div>
+//                 logo
+//             </div>
+
+//             <ul>
+//                 <SidebarLink Icon={HomeIcon} text="Home" />
+//                 <SidebarLink Icon={HomeIcon} text="Home" />
+//                 <SidebarLink Icon={HomeIcon} text="Home" />
+//                 <SidebarLink Icon={HomeIcon} text="Home" />
+//                 <SidebarLink Icon={HomeIcon} text="Home" />
+//                 <SidebarLink Icon={HomeIcon} text="Home" />
+//                 <SidebarLink Icon={HomeIcon} text="Home" />
+//                 {/* npm intall @heroicons/react/24/outline */}
+//             </ul>
+
+//             <div>
+//                 user info
+//             </div>
+//         </nav>
+//     </>
+//   )
+// }
+
+// function SidebarLink({ text, Icon }) {
+//     return (
+//         <>
+//         <li className='flex items-center text-xl' >
+//             <Icon className="h-7" />
+//             {text}
+//         </li>
+//         </>
+//     )
+// }
+
+
+
+//step 1
+// import React from 'react'
+
+// export default function Sidebar() {
+//   return (
+//     <>
+//     <nav>
+//         <div>
+//             Logo
+//         </div>
+
+//         <ul>
+//             <li>Home</li>
+//             <li>Home</li>
+//             <li>Home</li>
+//             <li>Home</li>
+//             <li>Home</li>
+//             <li>Home</li>
+//             <li>Home</li>
+//         </ul>
+
+//         <div>
+//             user info
+//         </div>
+//     </nav>
+//     </>
+//   )
+// }
