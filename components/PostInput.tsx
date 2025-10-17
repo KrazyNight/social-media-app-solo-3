3cd36b6006c6d2e78460432a9ce9d28b6d7c89c3
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-10-17 10:53:40 -0700

    Sidebar PostFeed Widgets do over part 3

--- components/PostInput.tsx
+++ components/PostInput.tsx
@@ -1,9 +1,51 @@
+import { CalendarIcon, ChartBarIcon, FaceSmileIcon, MapPinIcon, PhotoIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
 import React from 'react'
 
 export default function PostInput() {
   return (
     <>
-    <div>
+    <div className='flex p-3 space-x-6 border-b-2 '>
+      <Image
+      src={"/assets/busybee-logo2.png"}
+      width={44}
+      height={44}
+      alt='Logo'
+      className='w-11 h-11'
+
+      />
+
+      <div className='w-full  '>
+
+        <textarea className='
+        w-full min-h-[50px] text-lg
+        resize-none outline-none
+        border-b-2
+        '
+        placeholder="What's happening!? "
+        />
+
+        <div className='flex justify-between pt-5'>
+          <div className='flex space-x-2'>
+            <PhotoIcon className='w-5 h-5 text-[#f4af01] '/>
+            <ChartBarIcon className='w-5 h-5 text-[#f4af01] '/>
+            <FaceSmileIcon className='w-5 h-5 text-[#f4af01] '/>
+            <CalendarIcon className='w-5 h-5 text-[#f4af01] '/>
+            <MapPinIcon className='w-5 h-5 text-[#f4af01] '/>
+          </div>
+          <button className='
+          bg-[#f4af01] text-white text-sm
+          w-[80px] h-[36px]
+          rounded-full cursor-pointer
+
+          '>
+            Bumble
+          </button>
+        </div>
+      </div>
+      
+
+
 
     </div>     
     </>
